16a61be238bd5f405324b0b537486fb5940d2db4
tobiasprag <51359938+tobiasprag@users.noreply.github.com>
2023-05-18 15:06:40 +0200

    progress on html from json with JS
    Thanks to AI ;)

---
+++ json/script.js
@@ -0,0 +1,52 @@
+const menuButton = document.querySelector(".menu__icon");
+const menu = document.querySelector(".links");
+const menuLinks = document.querySelectorAll(".link");
+const overflow = document.querySelector("body");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("show-menu");
+  menu.classList.toggle("menu-open");
+  overflow.style.overflow = "hidden";
+
+  for (i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].classList.toggle("links-open");
+  }
+});
+
+fetch("students.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const classesList = document.getElementById("classes");
+
+    data.classes.forEach((classData) => {
+      const classWrapper = document.createElement("div");
+      classWrapper.classList.add("class");
+
+      const className = document.createElement("h2");
+      className.textContent = classData.className;
+
+      classWrapper.appendChild(className);
+
+      const studentsList = document.createElement("ul");
+      classData.students.forEach((student) => {
+        const studentWrapper = document.createElement("li");
+        studentWrapper.classList.add("student");
+
+        const name = document.createElement("a");
+
+        name.href =
+          "https://" +
+          student.studentName.replace(/\s/g, "") +
+          "." +
+          student.studentURL;
+        name.textContent = student.studentName + " " + student.studentURL;
+        studentWrapper.appendChild(name);
+
+        studentsList.appendChild(studentWrapper);
+      });
+
+      classWrapper.appendChild(studentsList);
+      classesList.appendChild(classWrapper);
+    });
+  })
+  .catch((error) => console.error(error));
